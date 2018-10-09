4962170a04797a9bbb2329562488abf5b9621f4f
Marcos <marcospernica@gmail.com>
2018-10-09 13:32:14 -0300

    Servidor NTP de AFIP agregado

--- instalar.sh
+++ instalar.sh
@@ -6,7 +6,7 @@ sudo rm /etc/chrony.conf
 sudo rm /var/log/chrony/measurements.log
 sudo touch /etc/chrony.conf
 sudo chmod 777 /etc/chrony.conf
-sudo echo 'pool time1.google.com iburst' >> /etc/chrony.conf
+sudo echo 'pool time.afip.gov.ar iburst' >> /etc/chrony.conf
 sudo sed -i -e '$a makestep 1.0 3' /etc/chrony.conf
 sudo sed -i -e '$a logdir /var/log/chrony' /etc/chrony.conf
 sudo sed -i -e '$a log measurements' /etc/chrony.conf
