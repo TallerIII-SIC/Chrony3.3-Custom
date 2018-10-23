aa2ee2e5db61aebe0ecd186ea13d06ba6e7f4f23
david <marcospernica@gmail.com>
2018-10-22 22:56:25 -0300

    Primera tanda de mediciones

--- .deps/ntp_io.d
+++ .deps/ntp_io.d
@@ -1,4 +1,4 @@
 ntp_io.o .deps/ntp_io.d: ntp_io.c config.h sysincl.h array.h ntp_io.h \
  ntp.h hash.h addressing.h ntp_core.h srcparams.h sources.h reports.h \
  sourcestats.h ntp_sources.h sched.h local.h logging.h conf.h reference.h \
- privops.h util.h candm.h ntp_io_linux.h
+ privops.h util.h candm.h
--- .deps/refclock_pps.d
+++ .deps/refclock_pps.d
@@ -1,3 +1,3 @@
 refclock_pps.o .deps/refclock_pps.d: refclock_pps.c config.h refclock.h \
  srcparams.h sources.h sysincl.h ntp.h hash.h reports.h addressing.h \
- sourcestats.h
+ sourcestats.h logging.h memory.h util.h candm.h
